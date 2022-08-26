5b482f4ae119edeae9e223024d9eeef8469fb9c8
patipan-patisampita <patipan.patisampita@gmail.com>
2022-08-26 15:10:31 +0700

    update

--- lib/gridview_card_page.dart
+++ lib/gridview_card_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'mycard.dart';
+
 class Menu {
   final String? title;
   final IconData? icon;
@@ -29,28 +31,19 @@ class GridViewCardPage extends StatelessWidget {
           crossAxisCount: menus.length,
           mainAxisSpacing: 10,
           crossAxisSpacing: 10,
-          children: List.generate(menus.length, (index) {
-            return Card(
-              color: menus[index].bkColor,
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
-                ),
-              ),
-              child: InkWell(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(menus[index].icon, size: 65, color: Colors.white),
-                    Text(menus[index].title!, style: TextStyle(color: Colors.white)),
-                  ],
-                ),
-                onTap: () {},
-              ),
-            );
-          }),
+          children: List.generate(
+            menus.length,
+            (index) {
+              return MyCard(
+                title: menus[index].title,
+                icon: menus[index].icon,
+                color: menus[index].bkColor,
+                onTap: () {
+                  debugPrint(menus[index].title);
+                },
+              );
+            },
+          ),
         ),
       ),
     );
